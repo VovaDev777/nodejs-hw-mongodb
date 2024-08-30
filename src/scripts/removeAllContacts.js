2ac7698908eaed26f3a45f69f67f71e61c5907b7
VovaDev777 <polvovater@gmail.com>
2024-08-30 17:04:31 +0200

    1

--- src/scripts/removeAllContacts.js
+++
@@ -1,8 +0,0 @@
-import { PATH_DB } from '../constants/contacts.js';
-import * as fs from 'node:fs/promises';
-
-export const removeAllContacts = async () => {
-    return await fs.writeFile(PATH_DB, JSON.stringify([]));
-};
-
-removeAllContacts();
