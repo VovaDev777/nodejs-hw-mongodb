e3e032c2fa83da1c3c4fb7132562f17372c01f0f
VovaDev777 <polvovater@gmail.com>
2024-08-30 18:13:08 +0200

    1

--- src/server.js
+++ src/server.js
@@ -1,38 +1,42 @@
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
-
-dotenv.config();
-const port = Number(process.env.PORT || 3000);
+import { env } from './utils/env.js';
 
 const setupServer = () => {
-    const app = express();
+  const app = express();
 
-    const logger = pino({
-        transport: {
-            target: "pino-pretty"
-        }
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
+  app.use(logger);
+  app.use(cors());
+  app.use(express.json());
+
+  app.get("/contacts", (req, res) => {
+    
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
     });
-    app.use(logger);
-    app.use(cors());
-    app.use(express.json());
+  });
 
-
-    app.use((req, res) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
     });
+  });
 
-    app.use((error, req, res, next) => {
-        res.status(500).json({
-            message: error.message
-        });
-    });
+  const port = Number(env('PORT', 3000));
 
-    app.listen(port, () => console.log(`Server is running on port ${process.env.PORT}`));
+  app.listen(port, () =>
+    console.log(`Server is running on port ${process.env.PORT}`),
+  );
 };
 
 export default setupServer;
