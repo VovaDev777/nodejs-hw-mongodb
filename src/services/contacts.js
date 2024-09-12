11c610e4784dd6173d5f133b76c56fe765dbe49f
VovaDev777 <polvovater@gmail.com>
2024-09-12 11:37:38 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,5 +3,3 @@ import Contacts from "../db/models/Contacts.js";
 export const getAllContacts = () => Contacts.find();
 
 export const getContactById = id => Contacts.findById(id);
-
-export const getContactById = id => Contacts.findById(id);
