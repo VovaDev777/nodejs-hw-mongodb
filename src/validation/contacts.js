f1294cac791829f4f01b0cbb16dcb4765ba64768
VovaDev777 <polvovater@gmail.com>
2024-09-12 13:27:00 +0200

    1

---
+++ src/validation/contacts.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const contactsAddSchema = Joi.object({
+    
+});
+
+
+// "name": "Vova",
+//             "phoneNumber": "+777777777",
+//             "email": "vasyl9@example.com",
+//             "isFavourite": false,
+//             "contactType": "home",
