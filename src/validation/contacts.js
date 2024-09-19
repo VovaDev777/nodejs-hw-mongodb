d39e36da883638200cca880de00299f2916f18cf
VovaDev777 <polvovater@gmail.com>
2024-09-19 13:13:29 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ import { contactTypeList } from "../constants/contacts.js";
 export const contactsAddSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string().min(3).max(20).required(),
+    email: Joi.string().min(3).max(20),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().min(3).max(20).valid(...contactTypeList).required(),
 });
