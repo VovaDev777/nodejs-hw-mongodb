b63e6a746a2ae4aa1ae656a3562551f0f88f9aa0
VovaDev777 <polvovater@gmail.com>
2024-10-07 11:53:48 +0200

    1

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -28,13 +28,13 @@ authRouter.post('/refresh', ctrlWrapper(authController.refreshController));
 authRouter.post('/logout', ctrlWrapper(authController.logoutController));
 
 authRouter.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
 );
 
 authRouter.post(
-  '/reset-password',
+  '/reset-pwd',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
 );
